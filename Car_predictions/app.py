fb803bc2b9417fe0b9384b26c20d2710ae522ee0
amoako419 <ghheskey@gmail.com>
2024-09-27 06:31:33 +0000

    ui built

---
+++ Car_predictions/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("Car price predictor")
+
+st.text("Fill in this forms.")
+
+brand = st.selectbox("Choose the Car's brand : ",("Toyota","Honda","Ford","Chevrolet","Hyundai"))
